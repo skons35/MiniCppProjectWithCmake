620a38ce0d73d1f164c0f6b833caa02a209f1ece
Vincent A <22142836+skons35@users.noreply.github.com>
2020-04-03 18:08:03 +0200

    - add comments in code

--- MiniProject_1/src/miniProj1.cpp
+++ MiniProject_1/src/miniProj1.cpp
@@ -1,6 +1,10 @@
 ﻿// miniProj1.cpp : Defines the entry point for the application.
 //
 
+//
+// add OpenCV headers you need here
+//
+
 #include "pseudoFunctions.h"
 
 using namespace std;
@@ -9,6 +13,9 @@ int main()
 {
 	cout << "miniproj1 started." << endl;
 	
+	// add your OpenCV's dependant call here, .... 
+	
+	// let's call our pseudo library function :
 	bool testRet = pseudoFunction();
 	
 	cout << "miniproj1 pseudoFunction() returns : "<< testRet << endl;
